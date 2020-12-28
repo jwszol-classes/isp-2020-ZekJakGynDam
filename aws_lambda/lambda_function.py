ee848ae78d1cb975029a96dfb2dea9be94b55a05
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2020-12-28 23:50:41 +0100

    Naprawiłem buga w Pawłowym producencie, poprawiłem lambda_function i dodałem skrypt testowy

--- aws_lambda/lambda_function.py
+++ aws_lambda/lambda_function.py
@@ -1,6 +1,8 @@
 import json
 import base64
 import reverse_geocode
+import datetime
+import boto3
 
 
 def get_decoded_data(record):
@@ -8,26 +10,36 @@ def get_decoded_data(record):
     return decoded_data
 
 
+def timestamp2datetime(timestamp):
+    datetime_ = datetime.datetime.fromtimestamp(timestamp)
+    datetime_formated = datetime_.strftime('%Y-%m-%d %H:%M:%S.%f')
+    return datetime_formated
+
+
 def create_table_item(airplane_data, delimiter="|"):
     airplane_data_split = airplane_data.split(delimiter)
+
+    datetime_formated = timestamp2datetime(float(airplane_data_split[1]))
+
     # We can add more data here
     item = {
-        "icao24":    airplane_data_split[0]
-        "timestamp": airplane_data_split[1]
-        "latitude":  airplane_data_split[2]
-        "longitude": airplane_data_split[3]
-        "heading":   airplane_data_split[4]
+        "icao24":    airplane_data_split[0],
+        "timestamp": float(airplane_data_split[1]),
+        "datetime":  datetime_formated,
+        "latitude":  float(airplane_data_split[2]),
+        "longitude": float(airplane_data_split[3]),
+        "heading":   float(airplane_data_split[4])
     }
     return item
 
 
-def is_above_country(item, country="Poland"):
-    latitude  = item["latitude"]
-    longitude = item["longitude"]
-    coordinates = ((latitude, longitude),)
+def is_above_country(item, country):
+    latitude  = float(item["latitude"])
+    longitude = float(item["longitude"])
+    coordinates = (latitude, longitude),
     result = reverse_geocode.search(coordinates)
     
-    if result[country]:
+    if result[0]["country"] == country:
         return True
     else:
         return False
@@ -39,17 +51,32 @@ def add_record(item, table):
 
 def lambda_handler(event, context):
     # TODO implement
-    dynamodb = boto3.resource('dynamodb')
-    airplane_table = dynamodb.Table('Airplanes')
 
+    # Set configuration variables
+    country = "Poland"
+
+    # Set DynamoDB variables
+    dynamodb             = boto3.resource('dynamodb')
+    airplane_table       = dynamodb.Table('Airplanes')
+    airplaneICAO24_table = dynamodb.Table('AirplanesICAO24')
+
+    # Main loop for each record
     for record in event["Records"]:
+        # Get and decode airplane data
         airplane_data = get_decoded_data(record)
-        item          = create_table_item(airplane_data)
 
-        if is_above_country(item) is False:
+        # Create items for tables
+        item = create_table_item(airplane_data)
+        # print(item)
+
+        # Check if airplane is above defined country
+        if is_above_country(item, country) is False:
+            print("Discarded", item)
             continue
 
+        # Update DynamoDB tables
         add_record(item, airplane_table)
+        add_record(item, airplaneICAO24_table)
     
     return {
         'statusCode': 200,
--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+import time
+import datetime
+from datetime import timezone
+print(time.time())
+print(datetime.datetime.now())
+print(datetime.datetime.now().replace(tzinfo=timezone.utc).timestamp())
+exit()
+
+
 import reverse_geocoder as rg
 coordinates = ((-37.81, 144.96),)
 print(rg.search(coordinates))
