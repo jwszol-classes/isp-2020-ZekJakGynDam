67b330e7a18ccba66e6b4a1312da6056bbefed3d
Damian <s165712@student.pg.edu.pl>
2021-01-03 00:20:11 +0100

    Naprawione, zaznaczone miejsce do zmiany na aws

--- .temp.py
+++ .temp.py
@@ -1,6 +1,4 @@
-print("aaa")
 import dash
-print("zzz")
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly
@@ -9,6 +7,11 @@ import plotly_express as px
 import plotly.graph_objects as go
 from opensky_api import OpenSkyApi
 import numpy as np
+
+import boto3
+from boto3.dynamodb.conditions import Key
+from query_dynamo import list_airplanes
+
 
 fig = go.Figure()
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -24,11 +27,12 @@ app.layout = html.Div(
         )
     ])
 )
-
 @app.callback(Output('live-update-graph', 'figure'),
               Input('interval-component', 'n_intervals'))
 def update_graph_live(n):
     ##tutaj nalezy zmienic na ciagniecie danych z AWS-a
+    res = list_airplanes()
+    print(res)  
     state=api.get_states(bbox=(49.0273953314, 54.8515359564, 14.0745211117, 24.0299857927)).states
 
     data = {
@@ -43,7 +47,7 @@ def update_graph_live(n):
         data['Latitude'].append (state[i].latitude  )
         data['ICAO'].append(state[i].icao24)
 
-    fig.update_traces(marker=go.scattermapbox.Marker( size=5, color="rgb(100, 100, 100)", opacity=0.5),mode='markers')
+    fig.update_traces(marker=go.scattermapbox.Marker( size=5, color="rgb(100, 100, 100)", opacity=0.5),mode='markers')#chamskie ale zamalowuje wszystkie stare punkty na szaro
 
     fig.add_trace(go.Scattermapbox(
         #name="Plane",
@@ -57,17 +61,15 @@ def update_graph_live(n):
         ),
         text=data['ICAO'],
         # label_bgcolor='rgb(200,200,200)',
-        hoverinfo='text'
-        # hoverlabel_bgcolor=
+        hoverinfo='text',
     ))
-    # print(fig.data.name)
-    # print(len(data))
+
 
     fig.update_layout(height=1000,showlegend=False)
-    fig.update_mapboxes(accesstoken="ACCESS_TOKEN",#do ogarniecia z mapboxa
+    fig.update_mapboxes(accesstoken="pk.eyJ1IjoiZGFhYW1pYW4iLCJhIjoiY2tqOHFnemZmNTB2eTJxc2NwbDR6bzNkciJ9.nPJVeE3p8ooX4vLrk9IMww",
      zoom=5.8,
      center=dict(lat=51.8,lon=21.1),
-     uirevision=len(data),#To jest takie troche dziwne, ale coś mu trzeba podać, bo inaczej resetuje widok - len data w tym przypadku zawsze wynosi 3 i sie nie powinno zmieniac
+     uirevision=len(data), #To jest takie troche dziwne, ale coś mu trzeba podać, bo inaczej resetuje widok - len data w tym przypadku zawsze wynosi 3 i sie nie powinno zmieniac
      style='dark',
     )
     return fig
