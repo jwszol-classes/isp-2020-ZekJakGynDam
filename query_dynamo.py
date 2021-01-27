4d1e2a98e54a7d03032b463c45acc35729541d23
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2021-01-28 00:05:02 +0100

    final view and query_dynamo correction

--- query_dynamo.py
+++ query_dynamo.py
@@ -4,7 +4,7 @@ import time
 import datetime
 
 dynamodb = boto3.resource("dynamodb")
-time_eps=390000
+time_eps=300
 
 # def read_last_row():
     # table = dynamodb.Table("Airplanes")
@@ -30,9 +30,9 @@ def list_airplanes():
     }
     for i in response['Items']:
         # print("I: ",i)
-        diff=datetime.datetime.now() - float(i['timestamp'])
-        # print("SEKUNDY: ", diff.seconds)
-        if diff.seconds < time_eps:
+        diff=datetime.datetime.now().timestamp() - float(i['timestamp'])
+        # print("SEKUNDY: ", diff)
+        if diff < time_eps:
             data['Latitude'].append(float(i['latitude']))
             data['Longitude'].append(float(i['longitude']))
             data['ICAO'].append(str(i['ICAO24']))
