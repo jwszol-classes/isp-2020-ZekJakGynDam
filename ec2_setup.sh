3b7ed2c6efd194e6d20b20d8e1f84db895a20e58
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2021-02-19 20:20:28 +0100

    ec2_setup.sh update

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -1,15 +1,16 @@
 mkdir Projects
 cd Projects
 
-install pip for python3
+git clone git@github.com:jwszol-classes/isp-2020-ZekJakGynDam.git
+cd isp-2020-ZekJakGynDam/
+git checkout -b simple_visualization
+git pull git@github.com:jwszol-classes/isp-2020-ZekJakGynDam.git simple_visualization
 
 mkdir get_pip
 curl https://bootstrap.pypa.io/get-pip.py -o get_pip/get-pip.py
 python3 get_pip/get-pip.py
+rm -r -f get_pip/
 python3 -m pip install virtualenv
-
-git clone git@github.com:jwszol-classes/isp-2020-ZekJakGynDam.git
-cd isp-2020-ZekJakGynDam/
 cp .vimrc ~/.vimrc
 
 python3 -m virtualenv venv
@@ -21,5 +22,4 @@ python setup.py install
 cd ../..
 rm -r -f opensky-api/
 
-pip install boto3
-pip install bs4
+pip install -r requirements.txt
