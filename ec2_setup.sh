348c5a2afc868c1decda7f4497ea197f3ebfb7ff
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2021-02-19 20:21:15 +0100

    ec2 setup script

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -1,11 +1,3 @@
-mkdir Projects
-cd Projects
-
-git clone git@github.com:jwszol-classes/isp-2020-ZekJakGynDam.git
-cd isp-2020-ZekJakGynDam/
-git checkout -b simple_visualization
-git pull git@github.com:jwszol-classes/isp-2020-ZekJakGynDam.git simple_visualization
-
 mkdir get_pip
 curl https://bootstrap.pypa.io/get-pip.py -o get_pip/get-pip.py
 python3 get_pip/get-pip.py
