38b8462a6d058317220e6bf851a4b48e96525e9a
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2021-02-19 20:33:35 +0100

    ec2 setup script

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -1,3 +1,17 @@
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt-get install unzip
+unzip awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
+rm -r -f ./aws
+mkdir ~/.aws
+touch ~/.aws/credentials
+touch ~/.aws/config
+echo [default] \
+region = us-east-1 \
+output = json > ~/.aws/config
+
+
 mkdir get_pip
 curl https://bootstrap.pypa.io/get-pip.py -o get_pip/get-pip.py
 python3 get_pip/get-pip.py
