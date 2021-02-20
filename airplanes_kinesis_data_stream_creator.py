162a1183f93571be345279d85392aeb7241b4d4a
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2021-02-20 12:33:16 +0100

    kinesis data stream auto creator

--- airplanes_kinesis_data_stream_creator.py
+++ airplanes_kinesis_data_stream_creator.py
@@ -1,9 +1,10 @@
 import os
 
 
-def create_kinesis_data_stream_airplanes(stream_name="kinesis_data_stream_airplanes", shard_count=2):
+def create_kinesis_data_stream_airplanes(stream_name="kinesis_data_stream_airplanes", shard_count=2, region="us-east-1"):
     command = 'aws kinesis create-stream \
     --stream-name ' + str(stream_name) + ' \
-    --shard-count ' + str(shard_count)
+    --shard-count ' + str(shard_count) + ' \
+    --region ' + str(region)
 
     os.system(command)
