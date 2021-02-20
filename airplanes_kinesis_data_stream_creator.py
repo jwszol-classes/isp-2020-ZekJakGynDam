f2da4cdeca4fbbaaeed8c871681072033432c5cb
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2021-02-20 12:28:40 +0100

    kinesis data stream auto creator correction

--- airplanes_kinesis_data_stream_creator.py
+++ airplanes_kinesis_data_stream_creator.py
@@ -1,9 +1,9 @@
 import os
 
 
-def create_kinesis_data_stream_airplanes(stream_name="kinesis_data_stream_airplanes", shard_count=2)
+def create_kinesis_data_stream_airplanes(stream_name="kinesis_data_stream_airplanes", shard_count=2):
     command = 'aws kinesis create-stream \
     --stream-name ' + str(stream_name) + ' \
     --shard-count ' + str(shard_count)
-    
+
     os.system(command)
